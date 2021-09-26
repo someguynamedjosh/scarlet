beb686db7bc6bdd25ce5cb41c8f45c4f408eec8c
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2021-09-26 15:30:56 -0700

    rover -> scarlet

--- scarlet/src/stage2/ingest/mod.rs
+++ scarlet/src/stage2/ingest/mod.rs
@@ -20,9 +20,6 @@ pub fn ingest(
     scopes: &[&Definitions],
     extra_defines: Definitions,
 ) -> Result<ItemId, String> {
-    // let (rover, god_type) = define_rover_item(env);
-    // let rover_def = ("rover".to_string(), rover);
-
     let mut ctx = Context::new(env).with_current_item_id(into);
     for scope in scopes {
         ctx = ctx.with_additional_scope(scope);
