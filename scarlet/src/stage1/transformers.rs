95abfa18a724df320e19f12c873a8628ec73ca5d
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2021-10-17 10:56:12 -0700

    asdf

--- scarlet/src/stage1/transformers.rs
+++ scarlet/src/stage1/transformers.rs
@@ -60,19 +60,24 @@ macro_rules! binary_operator {
     };
 }
 
+binary_operator!(Caret, "pow", "^");
 binary_operator!(Asterisk, "mul", "*");
 binary_operator!(Plus, "add", "+");
 
 fn build_transformers(precedence: u8) -> Vec<Box<dyn Transformer>> {
     match precedence {
         10 => vec![Box::new(Parentheses)],
+        61 => vec![Box::new(Caret)],
         70 => vec![Box::new(Asterisk)],
         80 => vec![Box::new(Plus)],
         _ => vec![],
     }
 }
 
-fn apply_transformer<'t>(to: &mut Vec<TokenTree<'t>>, transformer: &(impl Transformer + ?Sized)) {
+fn apply_transformer_ltr<'t>(
+    to: &mut Vec<TokenTree<'t>>,
+    transformer: &(impl Transformer + ?Sized),
+) {
     let mut index = 0;
     while index < to.len() {
         if transformer.should_be_applied_at(&to[index]) {
@@ -91,10 +96,36 @@ fn apply_transformer<'t>(to: &mut Vec<TokenTree<'t>>, transformer: &(impl Transf
     }
 }
 
+fn apply_transformer_rtl<'t>(
+    to: &mut Vec<TokenTree<'t>>,
+    transformer: &(impl Transformer + ?Sized),
+) {
+    let mut index = to.len();
+    while index > 0 {
+        index -= 1;
+        if transformer.should_be_applied_at(&to[index]) {
+            let result = transformer.apply(to, index);
+            if !result.replace_range.contains(&index) {
+                panic!(
+                    "Transformer wants to replace {:?}, \
+                    which does not contain the original index {}.",
+                    result.replace_range, index
+                );
+            }
+            index = *result.replace_range.start();
+            to.splice(result.replace_range, std::iter::once(result.with));
+        }
+    }
+}
+
 pub fn apply_transformers<'t>(to: &mut Vec<TokenTree<'t>>) {
     for precedence in 0..=u8::MAX {
         for transformer in build_transformers(precedence) {
-            apply_transformer(to, &*transformer);
+            if precedence % 2 == 0 {
+                apply_transformer_ltr(to, &*transformer);
+            } else {
+                apply_transformer_rtl(to, &*transformer);
+            }
         }
     }
 }
