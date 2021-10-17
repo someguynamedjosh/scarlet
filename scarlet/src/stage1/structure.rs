a29e0b9499c2b0f344257c75d6511b0592a0e605
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2021-10-17 06:39:28 -0700

    Better printing

--- scarlet/src/stage1/structure.rs
+++ scarlet/src/stage1/structure.rs
@@ -1,8 +1,10 @@
-use std::fmt::Debug;
+use std::fmt::{self, Debug, Formatter};
+
+use crate::util::indented;
 
 pub type Token<'i> = &'i str;
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq)]
 pub enum TokenTree<'i> {
     Token(Token<'i>),
     Group {
@@ -14,6 +16,43 @@ pub enum TokenTree<'i> {
         name: Token<'i>,
         body: Vec<TokenTree<'i>>,
     },
+}
+
+impl<'i> Debug for TokenTree<'i> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            TokenTree::Token(text) => write!(f, "{:?}", text),
+            TokenTree::Group { start, end, body } => {
+                write!(f, "group{}", start)?;
+                for tt in body {
+                    match f.alternate() {
+                        true => write!(f, "\n    {}", indented(&format!("{:#?}", tt)))?,
+                        false => write!(f, " {:?}", tt)?,
+                    }
+                }
+                match f.alternate() {
+                    true => write!(f, "\n{}", end),
+                    false => write!(f, " {}", end),
+                }
+            }
+            TokenTree::PrimitiveRule { name, body } => {
+                match f.alternate() {
+                    true => write!(f, "primitive{{\n    {:?}", name)?,
+                    false => write!(f, "primitive{{{:?}", name)?,
+                }
+                for tt in body {
+                    match f.alternate() {
+                        true => write!(f, "\n    {}", indented(&format!("{:#?}", tt)))?,
+                        false => write!(f, " {:?}", tt)?,
+                    }
+                }
+                match f.alternate() {
+                    true => write!(f, "\n}}"),
+                    false => write!(f, "}}"),
+                }
+            }
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
