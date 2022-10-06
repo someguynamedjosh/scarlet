9351601d0661a6f8a4a4ed41b96aab82e849480c
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2022-10-06 16:39:51 -0700

    Constructor type checks now

--- scarlet/src/definitions/member_access.rs
+++ scarlet/src/definitions/member_access.rs
@@ -1,12 +1,14 @@
 use std::{
     collections::HashMap,
     fmt::{self, Formatter},
+    ops::Index,
 };
 
 use itertools::Itertools;
 
 use super::{
-    builtin::DBuiltin, compound_type::DCompoundType, new_value::DNewValue, parameter::ParameterPtr,
+    builtin::DBuiltin, compound_type::DCompoundType, new_type::DNewType, new_value::DNewValue,
+    parameter::ParameterPtr,
 };
 use crate::{
     diagnostic::Diagnostic,
@@ -21,7 +23,7 @@ use crate::{
     },
 };
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum Member {
     Unknown,
     IndexIntoUserType(usize),
@@ -33,6 +35,7 @@ pub struct DMemberAccess {
     base: ItemPtr,
     member_name: String,
     member_index: Member,
+    r#type: Option<ItemPtr>,
 }
 
 impl CycleDetectingDebug for DMemberAccess {
@@ -59,7 +62,7 @@ impl ItemDefinition for DMemberAccess {
     }
 
     fn recompute_type(&self, _ctx: &mut QueryContext<TypeQuery>) -> <TypeQuery as Query>::Result {
-        todo!()
+        self.r#type.clone()
     }
 
     fn recompute_type_check(
@@ -71,10 +74,13 @@ impl ItemDefinition for DMemberAccess {
 
     fn reduce(&self, this: &ItemPtr, args: &HashMap<ParameterPtr, ItemPtr>) -> ItemPtr {
         let base = self.base.reduce(args);
-        if self.member_name == "new" {
+        if self.member_index == Member::Unknown {
+            todo!("Should have been resolved earlier.");
+        } else if self.member_index == Member::Constructor {
             if let Some(r#type) = base.downcast_definition::<DCompoundType>() {
                 if let Some(constructor) = r#type.constructor(&base) {
-                    return constructor;
+                    println!("{:#?}", constructor);
+                    return constructor.reduce(args);
                 }
             }
         }
@@ -88,6 +94,7 @@ impl ItemDefinition for DMemberAccess {
                 base,
                 member_name: self.member_name.clone(),
                 member_index: self.member_index,
+                r#type: self.r#type.as_ref().map(|t| t.reduce(args)),
             }
             .into_ptr()
         }
@@ -99,8 +106,8 @@ impl ItemDefinition for DMemberAccess {
                 .with_text_error(format!("Failed to determine type of base."))
                 .with_item_error(this),
         )?;
-        let r#type = r#type.reduce(&HashMap::new());
-        let downcast = r#type.downcast_definition::<DCompoundType>();
+        let type_ptr = r#type.reduce(&HashMap::new());
+        let downcast = type_ptr.downcast_definition::<DCompoundType>();
         if let Some(r#type) = downcast {
             let components = r#type.get_component_types();
             if components.len() > 1 {
@@ -115,14 +122,29 @@ impl ItemDefinition for DMemberAccess {
             let component = components.iter().next().unwrap().1;
             let index = if component.is_exactly_type() {
                 match &self.member_name[..] {
-                    "new" => Some(Member::Constructor),
+                    "new" => Some((Member::Constructor, self.base.ptr_clone())),
                     _ => None,
                 }
+            } else if let Some(base_type) = component.downcast_definition::<DNewType>() {
+                base_type
+                    .get_fields()
+                    .iter()
+                    .position(|x| x.0 == self.member_name)
+                    .map(|index| {
+                        (
+                            Member::IndexIntoUserType(index),
+                            base_type.get_fields()[index]
+                                .1
+                                .query_type(&mut Environment::root_query())
+                                .unwrap(),
+                        )
+                    })
             } else {
-                todo!()
+                None
             };
-            if let Some(index) = index {
+            if let Some((index, r#type)) = index {
                 self.member_index = index;
+                self.r#type = Some(r#type);
                 Ok(())
             } else {
                 Err(Diagnostic::new())
@@ -139,6 +161,7 @@ impl DMemberAccess {
             base,
             member_name,
             member_index: Member::Unknown,
+            r#type: None,
         }
     }
 }
